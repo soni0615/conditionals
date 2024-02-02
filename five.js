3b78a634ab11f06173d98bdd16a37ff7ab51d17a
soni dhamnani <118076827+soni0615@users.noreply.github.com>
2024-02-02 18:35:38 +0530

    Add files via upload

---
+++ five.js
@@ -0,0 +1,21 @@
+// Ascending order
+let x=1
+console.log("Printing Numbers from 1 to 25");
+while(x>0){
+       console.log(x);
+      x=x+1
+       if(x==26){
+              break
+       }
+}
+// descending order
+let y=25
+console.log("Printing Numbers from 25 to 1");
+while(y>0){
+        x=x-1
+        if(x==0){
+              break
+        }
+        console.log(x);
+}
+
---
+++ two.js
@@ -0,0 +1,13 @@
+let studentMarks=49
+if(studentMarks>=90){
+    console.log("Student Got A Grade");
+}
+else if(studentMarks>=70 && studentMarks<90){
+    console.log("Student Got A B Grade");
+}
+else if(studentMarks>=50 && studentMarks<70){
+    console.log("Student Got A C Grade");
+}
+else{
+    console.log("Student Got F Grade")
+}
